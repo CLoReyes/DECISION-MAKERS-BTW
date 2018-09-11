dbcb6f9bda16f83b051c50cff8e47bb5bc7dc30b
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2018-09-11 08:25:29 -0600

    name and question done, now onto the decision part

--- Decision_Maker/src/DECISIONMAKER.java
+++ Decision_Maker/src/DECISIONMAKER.java
@@ -5,7 +5,28 @@ public class DECISIONMAKER
 		
 		public static void main(String[] args)
 			{
+	//			name();
+				idkWhatthisIsyet();
+			}
+
+		private static void name()
+			{
+				Scanner nombre = new Scanner (System.in);
+				System.out.println("What is your name?");
+				String name = nombre.nextLine();
+				System.out.println("Well hello " + name + ". I have been built to make decisions for you!");
 				
 			}
+
+		private static void idkWhatthisIsyet()
+			{
+				Scanner questions = new Scanner (System.in);
+				System.out.println("You will never have to make another hard decision on your own ever again!");
+				System.out.println("Now, what question do you want answered?");
+				String question = questions.nextLine();
+				
+				System.out.println("What an interesting dilema you got there. But don't worry, I will help you out.");
+			}
+
 
 	}
